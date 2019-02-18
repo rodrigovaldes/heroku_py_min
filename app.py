479f1850c35aedabd79749497f42377dd56ec4d6
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-02-18 12:29:57 -0600

    2 try

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ print('Hello World')
 
 # Some calculations
 n = 5
-m = 4 
+m = 4
 t = n + m
 print(t)
+
+
+with open('something.txt', 'w') as file:
+    file.write('whatever')
+    print('saved file')
